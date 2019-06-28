3412a87a91371a30afcc3a6d5a8e6db50491669d
Brian Batchelder <brian@briansbrain.biz>
2019-06-28 16:59:23 -0700

    Update copyrights and change comments to "Airship".

--- segment-integration/src/main/java/com/urbanairship/extension/segment/UrbanAirshipIntegration.java
+++ segment-integration/src/main/java/com/urbanairship/extension/segment/UrbanAirshipIntegration.java
@@ -1,5 +1,5 @@
 /*
- Copyright 2016 Urban Airship and Contributors
+ Copyright Airship and Contributors
 */
 
 package com.urbanairship.extension.segment;
@@ -20,7 +20,7 @@ import java.util.HashSet;
 
 
 /**
- * Urban Airship Segment integration.
+ * Airship Segment integration.
  */
 public class UrbanAirshipIntegration extends Integration<UAirship> {
 
